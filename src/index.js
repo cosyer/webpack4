be2a946a90d8a613986e66afacc27be3cab24766
chenyu <535509852@qq.com>
2019-05-13 00:45:43 +0800

    升级webpack-cli到^3不然报错添加utils-pro进行测试

--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
-import _ from 'lodash';
-import Print from './print';
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Image from "./images/webpack.png"
-import './index.css'
+import _ from "lodash";
+import Print from "./print";
+import React from "react";
+import ReactDOM from "react-dom";
+import Image from "./images/webpack.png";
+import "./index.css";
 // import './index.less'
+import rook from "utils-pro";
+import { getOS } from "utils-pro";
+// const getOS = require("utils-pro/lib/getOS");
 
+console.log(111111, getOS());
+console.log(222222, rook.getOS());
 class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <img src={Image} alt='webpack' className="image" />
-                <button className="leesBox"></button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <img src={Image} alt="webpack" className="image" />
+        <button className="leesBox" />
+      </div>
+    );
+  }
 }
 
 function component() {
-    console.log(api_host)
-    let element = document.createElement('div');
+  console.log(api_host);
+  let element = document.createElement("div");
 
-    // lodash 是由当前 script 脚本 import 导入进来的
-    element.innerHTML = _.join(['Hello', ' webpack3'], '');
-    element.onclick = Print.bind(null, 'Hello webpack3!');
+  // lodash 是由当前 script 脚本 import 导入进来的
+  element.innerHTML = _.join(["Hello", " webpack3"], "");
+  element.onclick = Print.bind(null, "Hello webpack3!");
 
-    return element;
+  return element;
 }
-ReactDOM.render(<App />, document.getElementById("app"))
+ReactDOM.render(<App />, document.getElementById("app"));
 document.body.appendChild(component());
